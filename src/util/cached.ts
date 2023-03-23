16dc8f3040fbf8d5b5d39c732b5049eb005194ee
a <a>
2023-03-23 11:17:56 -0300

    dev

--- src/util/cached.ts
+++ src/util/cached.ts
@@ -1,15 +1,45 @@
+export const cachedSymbol = Symbol('cached')
+
 export function cached<This, Value>(
   value: () => Value,
   { kind, name }: ClassGetterDecoratorContext<This, Value>
 ) {
   if (kind === 'getter') {
     return function (this: This) {
-      const result = value.call(this)
-      Object.defineProperty(this, name, {
-        value: result,
-        writable: false
-      })
-      return result
+      if (!this[cachedSymbol]) this[cachedSymbol] = {}
+
+      const target = this
+
+      if (!this[cachedSymbol][name])
+        this[cachedSymbol][name] = {
+          value: undefined,
+          clear() {
+            this.value = undefined
+          },
+          refresh() {
+            this.value = value.call(target)
+          }
+        }
+
+      const cache = this[cachedSymbol][name]
+
+      if (cache.value === undefined) cache.refresh()
+
+      return cache.value
     }
   }
+
+  throw new Error(`"cached" can only decorate getters.`)
+}
+
+export const clearCached = <T, K extends keyof T>(target: T, property: K) => {
+  if (!target[cachedSymbol] || !target[cachedSymbol][property]) return
+
+  target[cachedSymbol][property].clear()
+}
+
+export const refreshCached = <T, K extends keyof T>(target: T, property: K) => {
+  if (!target[cachedSymbol] || !target[cachedSymbol][property]) return
+
+  target[cachedSymbol][property].refresh()
 }
