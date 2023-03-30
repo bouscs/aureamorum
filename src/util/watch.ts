5eca17d5806e311870ebcf2934ca9d6e9450a625
a <a>
2023-03-30 10:36:11 -0300

    dev

--- src/util/watch.ts
+++ src/util/watch.ts
@@ -1,21 +1,50 @@
 export const watch = <
   This,
   Value extends Record<string | symbol | number, any>
->(options: {
+>({
+  watchedProperties,
+  onPropertiesChange,
+  onSet
+}: {
   watchedProperties: (keyof Value)[]
   reflectChanges?: keyof This
-  // onChange?: (this: This, )
+  onPropertiesChange?: (this: This, value: Value) => void
+  onSet?: (this: This, value: Value) => void
 }) => {
   return function (
-    value: ((this: This) => Value) | ClassAccessorDecoratorTarget<This, Value>,
-    context:
-      | ClassAccessorDecoratorContext<This, Value>
-      | ClassGetterDecoratorContext<This, Value>
-  ) {
-    if (context.kind === 'accessor') {
-      value = value as ClassAccessorDecoratorTarget<This, Value>
+    value: ClassAccessorDecoratorTarget<This, Value>,
+    context: ClassAccessorDecoratorContext<This, Value>
+  ): ClassAccessorDecoratorResult<This, Value> {
+    return {
+      get(this: This) {
+        const target = this
+        const r = value.get.call(this)
 
-      return {}
-    }
+        const props = new Map()
+
+        for (const prop of watchedProperties) {
+          props.set(prop, r[prop])
+        }
+
+        Object.defineProperties(
+          r,
+          watchedProperties.reduce((acc, current) => {
+            return {
+              ...acc,
+              get [current]() {
+                return props.get(current)
+              },
+              set [current](value: any) {
+                props.set(current, value)
+
+                onPropertiesChange?.call(target, value.get.call(target))
+              }
+            }
+          }, {} as any)
+        )
+
+        return r
+      }
+    } as ClassAccessorDecoratorResult<This, Value>
   }
 }
