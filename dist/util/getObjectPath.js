bb9dfc5fe06e79222807d3f5ff2b59bc61a6d55d
Artur <busanix@gmail.com>
2023-09-20 15:45:38 -0300

    fix getObjectPath

--- dist/util/getObjectPath.js
+++ dist/util/getObjectPath.js
@@ -1,5 +1,9 @@
 export const getObjectPath = (obj, path) => {
+    if (!path)
+        return undefined;
     const pathArray = path.split('.');
+    if (obj === undefined)
+        return undefined;
     let current = obj;
     for (let i = 0; i < pathArray.length; i++) {
         if (current[pathArray[i]] === undefined) {
